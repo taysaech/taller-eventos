f5b4c189ba0f81bc90d5def1c3d1f65d30951953
taysaech <taay_echenique@hotmail.com>
2024-08-15 06:16:13 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 const miDiv= document.getElementById ('miDiv');
+const Boton=document.getElementById('Boton');
 
 
 miDiv.addEventListener ('click',function() { 
  alert ('Hola! Soy el div');
 })
+
+
+Boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
